1d108528f14e380cbe11eaf9a1c33d0d5f90dac9
gcolman <graeme.colman@appvia.io>
2023-02-23 15:28:42 +0000

    more

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const tc = require('@actions/tool-cache');
 
-try {
 
-  const wf = await tc.downloadTool('https://storage.googleapis.com/wayfinder-releases/v1.6.10/wf-cli-linux-amd64.tar.gz');
-  const wfBin = await tc.extractTar(wf, '/bin');
+async function run() {
 
-  const wfPath = await tc.cacheDir(wfBin, 'wf', '1.16.10');
-  core.addPath(wfPath);
- 
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  try {
+
+    const wf = await tc.downloadTool('https://storage.googleapis.com/wayfinder-releases/v1.6.10/wf-cli-linux-amd64.tar.gz');
+    const wfBin = await tc.extractTar(wf, '/bin');
+
+    const wfPath = await tc.cacheDir(wfBin, 'wf', '1.16.10');
+    core.addPath(wfPath);
+    await exec.exec('wf version');   
+  
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
